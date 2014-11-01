52b7f7906c117496db073ee837c45409d7bc9363
Touhid Mia <hmtm.cse@gmail.com>
2014-11-01 11:53:28 +0600

    Touhid Mia

--- configuration/compileMe.sh
+++ configuration/compileMe.sh
@@ -6,7 +6,7 @@ cd /root/nginx
 rm -rf nginx-1.6.2
 tar -xzf nginx-1.6.2.tar.gz
 cd /root/nginx/nginx-1.6.2
-./configure  --add-module=/root/nginx/nginx/module/Print_Me
+./configure  --add-module=/root/nginx/nginx/module/Print_Me_By_Input
 make
 rm -rf /usr/local/nginx/*
 make install
