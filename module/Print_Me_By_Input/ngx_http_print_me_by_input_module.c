5620d64270242b38686634aafd33fa7cd140768f
Touhid Mia <hmtm.cse@gmail.com>
2014-11-01 11:53:50 +0600

    Touhid Mia

--- module/Print_Me_By_Input/ngx_http_print_me_by_input_module.c
+++ module/Print_Me_By_Input/ngx_http_print_me_by_input_module.c
@@ -2,38 +2,49 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-
-static u_char  ngx_printable_message[] = "Yes I was print.... ha ha ha";
-static char *ngx_http_print_me_by_input(ngx_conf_t *cf, ngx_command_t *cmd, void *conf
+static ngx_int_t ngx_http_print_me_by_input(ngx_conf_t *cf);
 
 typedef struct {
     ngx_str_t   message;
 } ngx_http_print_me_by_input_loc_conf_t;
 
 
+
 static ngx_command_t  ngx_http_print_me_by_input_commands[] = {
 
   { ngx_string("print_me_by_input"),
-    NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
-    ngx_http_print_me_by_input,
-    0,
-    0,
+    NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_str_slot,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    offsetof(ngx_http_print_me_by_input_loc_conf_t, message),
     NULL },
     ngx_null_command
 };
 
+
+static void *
+ngx_http_print_me_by_input_create_loc_conf(ngx_conf_t *cf){
+    ngx_http_print_me_by_input_loc_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_print_me_by_input_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+    return conf;
+}
+
+
 static ngx_http_module_t  ngx_http_print_me_by_input_module_ctx = {
-  NULL,                          /* preconfiguration */
-  NULL,                          /* postconfiguration */
+  NULL,                                         /* preconfiguration */
+  ngx_http_print_me_by_input,                   /* postconfiguration */
 
-  NULL,                          /* create main configuration */
-  NULL,                          /* init main configuration */
+  NULL,                                         /* create main configuration */
+  NULL,                                         /* init main configuration */
 
-  NULL,                          /* create server configuration */
-  NULL,                          /* merge server configuration */
-
-  NULL,                          /* create location configuration */
-  NULL                           /* merge location configuration */
+  NULL,                                         /* create server configuration */
+  NULL,                                         /* merge server configuration */
+  ngx_http_print_me_by_input_create_loc_conf,   /* create location configuration */
+  NULL                                          /* merge location configuration */
 };
 
 
@@ -53,6 +64,9 @@ ngx_module_t ngx_http_print_me_by_input_module = {
 };
 
 static ngx_int_t ngx_http_print_me_by_input_handler(ngx_http_request_t *r){
+    
+  ngx_http_print_me_by_input_loc_conf_t *loc = ngx_http_get_module_loc_conf(r, ngx_http_print_me_by_input_module);
+    
   ngx_buf_t    *b;
   ngx_chain_t   out;
 
@@ -65,21 +79,29 @@ static ngx_int_t ngx_http_print_me_by_input_handler(ngx_http_request_t *r){
   out.buf = b;
   out.next = NULL;
 
-  b->pos = ngx_printable_message;
-  b->last = ngx_printable_message + sizeof(ngx_printable_message);
+  b->pos = loc->message.data;
+  b->last = loc->message.data + loc->message.len;
   b->memory = 1;
   b->last_buf = 1;
 
   r->headers_out.status = NGX_HTTP_OK;
-  r->headers_out.content_length_n = sizeof(ngx_printable_message);
+  r->headers_out.content_length_n = loc->message.len;
   ngx_http_send_header(r);
 
   return ngx_http_output_filter(r, &out);
 }
 
-static char *ngx_http_print_me_by_input(ngx_conf_t *cf, ngx_command_t *cmd, void *conf){
-  ngx_http_core_loc_conf_t  *clcf;
-  clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-  clcf->handler = ngx_http_print_me_by_input_handler;
-  return NGX_CONF_OK;
+
+
+static ngx_int_t ngx_http_print_me_by_input(ngx_conf_t *cf){ 
+    ngx_http_handler_pt        *h;
+    ngx_http_core_main_conf_t  *cmcf;
+    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+    if (h == NULL) {
+        return NGX_ERROR;
+    }
+
+    *h = ngx_http_print_me_by_input_handler;
+    return NGX_OK;
 }
